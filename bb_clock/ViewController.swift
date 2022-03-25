d02d46397eb5c8d34479b92b20d6875d6c232adb
Yesserkepov <yeserkepov@gmail.com>
2022-03-25 16:39:14 +0600

    Initial commit

---
+++ bb_clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bb_clock
+//
+//  Created by Даурен on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
